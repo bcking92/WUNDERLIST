c2b8aac87b6fff6d00dec753449e4f07f3324fb2
Kim_ByungChul <bcking92@gmail.com>
2019-09-11 09:55:56 +0900

    add title filter

--- WUNDERLIST/todos/views.py
+++ WUNDERLIST/todos/views.py
@@ -13,10 +13,12 @@ def new(request):
     return render(request, 'todos/new.html')
 
 def create(request):
-    print("request:", request.POST.get('title'))
-    print("request:", request.POST.get('due_date'))
+    # print("request:", request.POST.get('title'))
+    # print("request:", request.POST.get('due_date'))
     title = request.POST.get('title')
     due_date = request.POST.get('due-date')
+    if len(title) >= 100:
+        return render(request, '형님~~~ 할일이 너~~무나 길어요 100자 이하로 입력해주세요!')
     Todo.objects.create(title=title, due_date=due_date)
     return redirect('todos:index')
 
